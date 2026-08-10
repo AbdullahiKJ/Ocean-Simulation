af58a3e92d5e16e046c17a9279f398312172d4cd
AbdullahiKJ <154251356+AbdullahiKJ@users.noreply.github.com>
2026-08-10 13:57:27 +0100

    feat: create initial vessel controller, add cinemachine package

--- Assets/Scripts/Vessel/VesselController.cs
+++ Assets/Scripts/Vessel/VesselController.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class VesselController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] float acceleration = 10f;
+    [SerializeField] float maxSpeed = 10f;
+    [SerializeField] float turnSpeed = 60f;
+
+    Rigidbody rb;
+
+    float movementX;
+    float movementY;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Forward/backward movement
+        Vector3 forwardForce = transform.forward * movementY * acceleration;
+        rb.AddForce(forwardForce, ForceMode.Acceleration);
+
+        // Limit horizontal speed
+        Vector3 horizontalVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        if (horizontalVelocity.magnitude > maxSpeed)
+        {
+            horizontalVelocity = horizontalVelocity.normalized * maxSpeed;
+
+            rb.linearVelocity = new Vector3(
+                horizontalVelocity.x,
+                rb.linearVelocity.y,
+                horizontalVelocity.z
+            );
+        }
+
+        // Steering
+        if (Mathf.Abs(movementY) > 0.01f)
+        {
+            float turn = movementX * turnSpeed * Time.fixedDeltaTime;
+            rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, turn, 0f));
+        }
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        Vector2 movementVector = inputValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 }
